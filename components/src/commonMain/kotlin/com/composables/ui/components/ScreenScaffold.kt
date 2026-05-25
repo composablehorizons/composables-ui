e7c7d94a5e6ac47f104f01c19d1b4a2db34e4c41
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-25 13:32:04 +0700

    WIP

--- components/src/commonMain/kotlin/com/composables/ui/components/ScreenScaffold.kt
+++ components/src/commonMain/kotlin/com/composables/ui/components/ScreenScaffold.kt
@@ -3,7 +3,6 @@ package com.composables.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,8 +23,7 @@ fun ScreenScaffold(
         Box(
             modifier = modifier
                 .fillMaxSize()
-                .background(backgroundColor)
-                .navigationBarsPadding(),
+                .background(backgroundColor),
         ) {
             content()
         }
