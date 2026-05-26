6f4c456789917673d88a9d958712d2e2de904ed5
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-26 10:12:06 +0700

    WIP

--- sample/src/jvmMain/kotlin/com/composables/ui/sample/Main.kt
+++ sample/src/jvmMain/kotlin/com/composables/ui/sample/Main.kt
@@ -1,7 +1,23 @@
 package com.composables.ui.sample
 
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
+import java.awt.Dimension
 
 fun main() = singleWindowApplication(title = "Sample App") {
+    val density = LocalDensity.current
+    val minimumWidth = with(density) { 340.dp.roundToPx() }
+
+    DisposableEffect(window, minimumWidth) {
+        val previousMinimumSize = window.minimumSize
+        window.minimumSize = Dimension(minimumWidth, previousMinimumSize.height)
+
+        onDispose {
+            window.minimumSize = previousMinimumSize
+        }
+    }
+
     SocialApp()
 }
