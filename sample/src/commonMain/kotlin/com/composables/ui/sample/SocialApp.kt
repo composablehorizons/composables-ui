73d1e2caff9c79729335e8cb7454ed93cd376eef
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-28 11:27:33 +0700

    WIP

--- sample/src/commonMain/kotlin/com/composables/ui/sample/SocialApp.kt
+++ sample/src/commonMain/kotlin/com/composables/ui/sample/SocialApp.kt
@@ -48,11 +48,31 @@ import com.composables.ui.theme.background
 import com.composables.ui.theme.colors
 import com.composeunstyled.currentWidthBreakpoint
 import com.composeunstyled.theme.Theme
+import kotlinx.serialization.Serializable
 
 private const val NavigationTransitionDurationMillis = 350
 private const val NavigationParallaxDivisor = 5
 private const val NavigationDimmedAlpha = 0.86f
 private val NavigationTransitionEasing = CubicBezierEasing(0.32f, 0.72f, 0f, 1f)
+
+@Serializable
+data object SocialFeedRoute
+
+@Serializable
+data object SearchRoute
+
+@Serializable
+data object NewPostRoute
+
+@Serializable
+data object ActivityRoute
+
+@Serializable
+data class PostDetailsRoute(val postId: String)
+
+@Serializable
+data class ProfileRoute(val profileId: String)
+
 
 @Composable
 fun SocialApp() {
