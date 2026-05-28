d2ba28d42ac5d9106e45fbb9a3b885be4bdc5c0f
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-28 18:08:42 +0700

    WI

--- sample/src/commonMain/kotlin/com/composables/ui/sample/components/MobileToolbar.kt
+++ sample/src/commonMain/kotlin/com/composables/ui/sample/components/MobileToolbar.kt
@@ -14,7 +14,6 @@ fun MobileToolbar() {
 
     Toolbar(
         leading = {
-            //
             AvatarButton(
                 url = authenticatedUser.avatarUrl,
                 onClick = {
